94e32804e3709b8aae720f06592e65ffad53a492
ceafdc <carloseaf@gmail.com>
2014-05-16 17:39:58 -0300

    Initial Commit

---
+++ DataStructures/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  DataStructures
+//
+//  Created by Gonzo Fialho on 5/16/14.
+//  Copyright (c) 2014 CEAFDC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
